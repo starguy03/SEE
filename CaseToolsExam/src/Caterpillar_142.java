c02e62c5a178fdf6c335055cf49f846890201499
admin <admin@Bcalab3-m16>
2022-06-14 09:14:30 +0530

    First Commit.

---
+++ CaseToolsExam/src/Caterpillar_142.java
@@ -0,0 +1,15 @@
+
+public class Caterpillar_142 {
+	int moves , cabbages;
+	Caterpillar_142(int moves , int cabbages)
+	{
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
